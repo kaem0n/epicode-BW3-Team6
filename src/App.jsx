68962b8dd3231870f9547c9d3e0c0a1e08727f7e
Liborio Daniele Frasca <frascaliboriodaniele@gmail.com>
2024-02-19 14:42:14 +0100

    footer-resources

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-icons/font/bootstrap-icons.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.min.css";
+// import dei component
+import MyFooter from "./components/MyFooter";
+import Resources from "./components/Resources";
 
 function App() {
-  return <div></div>
+  return (
+    <>
+      <Resources />
+      <MyFooter />
+    </>
+  );
 }
 
-export default App
+export default App;
 
 // Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWQzMTIxMzI0ZjYwNTAwMTkzN2Q0NWMiLCJpYXQiOjE3MDgzMzE1NDAsImV4cCI6MTcwOTU0MTE0MH0.Zl9ZBSk3lglgtHuX1aKTRzEJzPZ3CRCArwETLUu8CII
