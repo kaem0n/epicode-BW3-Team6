a2688a35c1f317530116635cfe0d2951bc6d623e
Riccardo Marchetti <marchettiriccardo04@gmail.com>
2024-02-19 16:58:27 +0100

    Person, Profile, Suggestions

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-icons/font/bootstrap-icons.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.min.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import Suggestions from "./components/Suggestions";
+import { Button, Card } from "react-bootstrap";
+import ProfileSettings from "./components/ProfileSettings";
+import Persons from "./components/Persons";
 
 function App() {
-  return <div></div>
+  return (
+    <div>
+      <Suggestions />
+      <ProfileSettings />
+      <Persons />
+    </div>
+  );
 }
 
-export default App
+export default App;
 
 // Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWQzMTIxMzI0ZjYwNTAwMTkzN2Q0NWMiLCJpYXQiOjE3MDgzMzE1NDAsImV4cCI6MTcwOTU0MTE0MH0.Zl9ZBSk3lglgtHuX1aKTRzEJzPZ3CRCArwETLUu8CII
