34d47dab8a043b2a4c13c7e16e21bd6f3a2c782f
Antonio Ruggia Piquer <149959739+kaem0n@users.noreply.github.com>
2024-02-19 18:40:42 +0100

    setup profile card

--- src/components/MyNavBar.jsx
+++ src/components/MyNavBar.jsx
@@ -80,15 +80,7 @@ const MyNavBar = () => {
               }
               id="collapsible-nav-dropdown"
             >
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
+              <div className="px-2">test</div>
             </NavDropdown>
             <Nav.Link
               onClick={openCanvas}
