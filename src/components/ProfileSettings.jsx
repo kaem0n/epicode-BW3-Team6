0349c4d54e7ac9e65f4fa47e86fb5ec62f9d1304
Antonio Ruggia Piquer <149959739+kaem0n@users.noreply.github.com>
2024-02-20 15:24:44 +0100

    polished profile settings modal

--- src/components/ProfileSettings.jsx
+++ src/components/ProfileSettings.jsx
@@ -16,17 +16,16 @@ const ProfileSettings = () => {
                   className="border-0 bg-transparent "
                   onClick={() => setLgShow(true)}
                 >
-                  <i className="bi bi-pencil"></i>
+                  <i className="bi bi-pencil fs-5"></i>
                 </Button>
                 <Modal
                   className="mt-5"
                   size="lg"
                   show={lgShow}
                   onHide={() => setLgShow(false)}
-                  aria-labelledby="example-modal-sizes-title-lg"
                 >
                   <Modal.Header closeButton>
-                    <Modal.Title id="example-modal-sizes-title-lg">
+                    <Modal.Title className="fs-5">
                       Impostazioni lingua del profilo
                     </Modal.Title>
                   </Modal.Header>
@@ -39,7 +38,7 @@ const ProfileSettings = () => {
                               <i className="bi bi-globe"></i>
                             </div>
                           </Col>
-                          <Col xs={11} sm={10}>
+                          <Col xs={11} sm={10} className="fs-7">
                             Aggiungi più di una lingua al tuo profilo, cosi sarà
                             più facile trovarti.
                           </Col>
@@ -50,7 +49,7 @@ const ProfileSettings = () => {
                           <Col xs={2} sm={2}>
                             <i className="bi bi-eyeglasses"></i>
                           </Col>
-                          <Col xs={11} sm={10}>
+                          <Col xs={11} sm={10} className="fs-7">
                             Se disponibile, mostreremo il tuo profilo nella
                             lingua di chi lo visita. Se non c&apos;è
                             corrispondenza, verrà mostrato il tuo profilo
@@ -60,7 +59,7 @@ const ProfileSettings = () => {
                       </Col>
                     </Row>
                   </Modal.Body>
-                  <Modal.Body>
+                  <Modal.Body className="p-4">
                     <h6
                       className="mb-3"
                       style={{ color: 'gray', fontWeight: '500' }}
@@ -69,7 +68,10 @@ const ProfileSettings = () => {
                     </h6>
                     <Row>
                       <div>
-                        <Button className="bg-transparent btn-modal-italiano py-1 me-2 ">
+                        <Button
+                          variant="secondary"
+                          className="bg-transparent btn-modal-italiano py-1 me-2 "
+                        >
                           Italiano
                         </Button>
                         <Button className="py-1 bg-transparent btn-add-language mt-xs-3">
@@ -78,15 +80,19 @@ const ProfileSettings = () => {
                       </div>
                     </Row>
                     <Row className="mt-3">
-                      <Col xs={1} className="d-flex justify-content-center">
-                        <i className="bi bi-lightbulb"></i>
-                      </Col>
-                      <Col xs={10}>
-                        <p>
+                      <Col xs={12} className="d-flex">
+                        <i className="fa-regular fa-lightbulb ms-1 me-3 mt-1"></i>
+                        <p className="m-0 fs-7 lh-sm">
                           Puoi avere un solo profilo per lingua. Quando cambi il
                           profilo nella tua lingua principale, le sezioni vuote
                           verranno compilate usando la tua precedente lingua
-                          principale.
+                          principale. Scopri di più su come{' '}
+                          <a
+                            href="#"
+                            className="fw-semibold link-underline link-underline-opacity-0 link-underline-opacity-100-hover"
+                          >
+                            creare profili in un’altra lingua.
+                          </a>
                         </p>
                       </Col>
                     </Row>
@@ -97,7 +103,7 @@ const ProfileSettings = () => {
               <div className="d-flex align-items-center justify-content-between ">
                 <Card.Title>Public profile & URL</Card.Title>
                 <Button className="border-0 bg-transparent ">
-                  <i className="bi bi-pencil"></i>
+                  <i className="bi bi-pencil fs-5"></i>
                 </Button>
               </div>
               <Card.Text>www.link.profilo</Card.Text>
