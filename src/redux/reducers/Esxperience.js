cb750b60f55e069c472b63193f2562fdad69d727
Rachele-Trimboli <146757214+Rachele-Trimboli@users.noreply.github.com>
2024-02-20 17:05:55 +0100

    Experience put, delete, post

---
+++ src/redux/reducers/Esxperience.js
@@ -0,0 +1,55 @@
+import {
+  DELETE_EXPERIENCE,
+  MY_EXPERIENCE,
+  NEW_EXPERIENCE,
+  UPDATE_EXPERIENCE,
+} from "../actions/ProfileSection";
+
+const initialState = {
+  experiences: [],
+  newExperience: {
+    role: "",
+    company: "",
+    startDate: "",
+    endDate: "",
+    description: "",
+    area: "",
+  },
+};
+
+export const myExperience = (state = initialState, action) => {
+  switch (action.type) {
+    case MY_EXPERIENCE:
+      return {
+        ...state,
+        experiences: [...state.experiences, action.payload],
+      };
+
+    case NEW_EXPERIENCE:
+      return {
+        ...state,
+        experiences: [...state.experiences, action.payload],
+      };
+
+    case DELETE_EXPERIENCE:
+      return {
+        ...state,
+        experiences: state.experiences.filter(
+          (experience) => experience.id !== action.payload
+        ),
+      };
+
+    case UPDATE_EXPERIENCE:
+      return {
+        ...state,
+        experiences: state.experiences.map((experience) =>
+          experience.id === action.payload.id ? action.payload : experience
+        ),
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default myExperience;
