af2d6567705a55764342b9c9432fe30f3b2262b3
Liborio Daniele Frasca <frascaliboriodaniele@gmail.com>
2024-02-21 19:08:46 +0100

    mod

--- src/components/ProfileSidebar.jsx
+++ src/components/ProfileSidebar.jsx
@@ -1,6 +1,5 @@
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
-import HomeShowMore from "./HomeShowMore";
 
 const ProfileSidebar = () => {
   return (
