243bd4977b33aa553d8fb016b89d23d7877b9c79
Liborio Daniele Frasca <frascaliboriodaniele@gmail.com>
2024-02-21 18:06:14 +0100

    mod

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,17 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.min.css';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import { Col, Container, Row } from 'react-bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.min.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { Col, Container, Row } from "react-bootstrap";
 
 //import pagine
-import CreateHomePost from './CreateHomePost';
-import MainHomePost from './MainHomePost';
-import NewsSidebar from './NewsSidebar';
-import HomeFooter from './HomeFooter';
-import ProfileSidebar from './ProfileSidebar';
-import Suggestions from './Suggestions';
-import HomeShowMore from './HomeShowMore';
+import CreateHomePost from "./CreateHomePost";
+import MainHomePost from "./MainHomePost";
+import NewsSidebar from "./NewsSidebar";
+import HomeFooter from "./HomeFooter";
+import ProfileSidebar from "./ProfileSidebar";
+import Suggestions from "./Suggestions";
+import HomeShowMore from "./HomeShowMore";
 
 const Home = () => (
   <Container className="mt-5">
