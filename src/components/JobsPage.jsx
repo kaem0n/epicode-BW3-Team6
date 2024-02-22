3f3f00b1b99e6c4df68214275d5a566e4a0f3326
francescosileoni <francesco.sile@gmail.com>
2024-02-22 15:23:32 +0100

    sidebar jobs

--- src/components/JobsPage.jsx
+++ src/components/JobsPage.jsx
@@ -1,28 +1,28 @@
-import JobsSidebar from './jobs_components/JobsSidebar'
-import { Col, Container, Row } from 'react-bootstrap'
-import SearchJobs from './jobs_components/SearchJobs'
-import OpenJobs from './jobs_components/OpenJobs'
-import Jobs from './jobs_components/Jobs'
+import JobsSidebar from './jobs_components/JobsSidebar';
+import { Col, Container, Row } from 'react-bootstrap';
+import SearchJobs from './jobs_components/SearchJobs';
+import OpenJobs from './jobs_components/OpenJobs';
+import HomeFooter from './HomeFooter';
 
 const JobsPage = () => {
   return (
     <>
-      <Container className="mt-5" fluid="xl">
-        <Row className="mb-3">
-          <Col xs={12} md={2} lg={3}>
+      <Container className="mt-5">
+        <Row className="justify-content-end">
+          <Col xs={12} md={4} lg={3}>
             <JobsSidebar />
           </Col>
-          <Col xs={12} md={6} lg={5} xl={6}>
+          <Col xs={12} md={8} lg={5} xl={6}>
             <SearchJobs />
           </Col>
-          <Col xs={12} md={4} lg={4} xl={3}>
+          <Col xs={12} md={8} lg={4} xl={3}>
             <OpenJobs />
+            <HomeFooter />
           </Col>
         </Row>
-        <Jobs />
       </Container>
     </>
-  )
-}
+  );
+};
 
-export default JobsPage
+export default JobsPage;
--- src/components/jobs_components/JobsSidebar.jsx
+++ src/components/jobs_components/JobsSidebar.jsx
@@ -1,35 +1,57 @@
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-// import Navbar from 'react-bootstrap/Navbar';
 
 import Dropdown from 'react-bootstrap/Dropdown';
-// import Nav from 'react-bootstrap/Nav';
-// import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const JobsSidebar = () => {
   return (
     <>
-      <Card className="mt-2">
+      <Card className="mt-2 mb-2" style={{ fontSize: '1.1rem' }}>
         <Card.Body className="ps-0 pe-0">
-          <div className="d-flex justify-content-between align-items-center">
-            <div className="fs-7">
-              <i className="bi bi-bookmark-fill"></i> Le mie offerte di lavoro
-            </div>
-            <div className="d-md-none">
+          <div className="d-flex justify-content-between   align-items-center">
+            <ul className="list-unstyled m-0 p-0 ps-3">
+              <li className="mb-0 mb-md-2">
+                <i className="bi bi-bookmark-fill pe-1 "></i>{' '}
+                <span>Le mie offerte di lavoro</span>
+              </li>
+              <div className="d-none d-md-block pe-1">
+                <li className="mb-2">
+                  <i className="bi bi-list-ul pe-1"></i> <span>Preferenze</span>
+                </li>
+                <li className="mb-2">
+                  <i className="bi bi-play-btn-fill pe-1"></i>{' '}
+                  <span>Indicazioni per chi cerca lavoro</span>
+                </li>
+                <li className="mb-2">
+                  <i className="bi bi-gear-fill pe-1"></i>{' '}
+                  <span>Impostazioni candidatura </span>
+                </li>
+              </div>
+            </ul>
+            <div className="d-md-none ">
               <Dropdown>
                 <Dropdown.Toggle
                   id="dropdown-basic"
-                  className="btn-homepost text-secondary"
+                  className="btn-homepost text-secondary fw-bold me-2"
                 >
                   Altro
                 </Dropdown.Toggle>
                 <Dropdown.Menu>
-                  <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                  <Dropdown.Item className="fw-bold" href="#/action-1">
+                    {' '}
+                    <i className="bi bi-list-ul pe-2 fw-bold"></i>Preferenze
+                  </Dropdown.Item>
                   <Dropdown.Item href="#/action-2">
-                    Another action
+                    <i className="bi bi-clipboard2-check pe-2"></i>
+                    Valutazione delle referenze
                   </Dropdown.Item>
                   <Dropdown.Item href="#/action-3">
-                    Something else
+                    <i className="bi bi-play-btn-fill pe-2"></i>
+                    Indicazioni per chi cerca lavoro
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">
+                    <i className="bi bi-gear-fill pe-2"></i>
+                    Impostazioni candidatura
                   </Dropdown.Item>
                 </Dropdown.Menu>
               </Dropdown>
@@ -37,7 +59,9 @@ const JobsSidebar = () => {
           </div>
         </Card.Body>
       </Card>
-      <Button>Pubblica offerta gratutita</Button>
+      <div className="d-md-block d-lg-block d-none text-center">
+        <Button className="btn-jobsidebar">Pubblica offerta gratutita</Button>
+      </div>
     </>
   );
 };
