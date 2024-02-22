58aedaf5b9fed23a961e1ab21ee3c970a6ac77b5
Rachele-Trimboli <146757214+Rachele-Trimboli@users.noreply.github.com>
2024-02-22 12:50:09 +0100

    .

--- src/components/jobs_components/Jobs.jsx
+++ src/components/jobs_components/Jobs.jsx
@@ -1,9 +1,6 @@
 import { useState } from "react";
 import { Button, Card, Col, Row, Spinner } from "react-bootstrap";
 import { useSelector } from "react-redux";
-import { useState } from "react";
-import { Button, Card, Col, Row, Spinner } from "react-bootstrap";
-import { useSelector } from "react-redux";
 
 const Jobs = () => {
   const [jobSelected, setJobSelected] = useState(null);
